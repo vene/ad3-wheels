be6090d1bcb07e406aebc85de371cfabc41b2ac2
Vlad Niculae <vlad@vene.ro>
2016-12-21 17:45:25 -0500

    Setting up multibuild for ad3

---
+++ README.md
@@ -0,0 +1,1 @@
+building wheels for ad3
---
+++ config.sh
@@ -0,0 +1,16 @@
+# Define custom utilities
+# Test for OSX with [ -n "$IS_OSX" ]
+
+function pre_build {
+    # Any stuff that you need to do before you start building the wheels
+    # Runs in the root directory of this repository.
+    :
+}
+
+function run_tests {
+    # Runs tests on installed distribution from an empty directory
+    python --version
+    nosetests --exe -v ad3
+    if [ -n "$IS_OSX" ]; then  # Run 32-bit tests on dual arch wheel
+        arch -i386 nosetests --exe -v ad3
+    fi
